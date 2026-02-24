57e28710537387ea39f18835137166c9d080b353
sajithh-007 <sajithsuresh2007@gmail.com>
2026-02-24 10:07:43 +0530

    UC3 Done

--- src/PrintOops.java
+++ src/PrintOops.java
@@ -8,4 +8,4 @@ public class PrintOops {
         System.out.println(String.join(" ","  *  *  ","  *  *   ","  *       ","     *"));
         System.out.println(String.join(" ","   **   ","   **    ","  *       ","  **"));
     }
-}
+} 
